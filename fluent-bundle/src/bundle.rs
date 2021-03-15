e02f0cc4b9640a8128f417a8fdeb834a3025a32b
Adamaq01 <adamthibert01@gmail.com>
2021-03-16 00:03:35 +0100

    Fix compilation issues

--- fluent-bundle/src/bundle.rs
+++ fluent-bundle/src/bundle.rs
@@ -532,7 +532,7 @@ impl<R, M> FluentBundle<R, M> {
     /// [FTL syntax guide]: https://projectfluent.org/fluent/guide/functions.html
     pub fn add_function<F>(&mut self, id: &str, func: F) -> Result<(), FluentError>
     where
-        F: Fn(&'a [FluentValue<'a>], &'a FluentArgs<'a>) -> FluentValue<'a> + Sync + Send + 'static,
+        F: for<'a> Fn(&'a [FluentValue<'a>], &'a FluentArgs<'a>) -> FluentValue<'a> + Sync + Send + 'static,
     {
         match self.entries.entry(id.to_owned()) {
             HashEntry::Vacant(entry) => {
